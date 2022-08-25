806eba52234752b7522339d1a13c6b3aa355afaf
pyojunbom <xenonluv@gist.ac.kr>
2022-08-25 23:17:00 +0900

    update25일

--- lib/screen/weather_screen.dart
+++ lib/screen/weather_screen.dart
@@ -32,26 +32,7 @@ class _WeatherScreenState extends State<WeatherScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                '$cityName',
-                style: TextStyle(fontSize: 30.0),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                '$temp2',
-                style: TextStyle(fontSize: 30.0),
-              )
-            ],
-          ),
-        ),
-      ),
+
     );
   }
 }
